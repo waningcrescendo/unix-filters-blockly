3a2277975bdb8c97a573ffacb83f540f865b247c
Raphaela Parreno <122368672+waningcrescendo@users.noreply.github.com>
2025-05-19 09:42:47 +0200

    fix: deleted useless files

--- src/exercises/ex01/index.js
+++
@@ -1,9 +0,0 @@
-export function initExercise (core, config) {
-  core.loadToolbox(config.toolbox)
-  core.loadStartWorkspace(config.startXml)
-  document.querySelector('#run').addEventListener('click', () => {
-    const code = core.generateCode()
-    const result = runUserCode(code, config.tests)
-    showResult(result)
-  })
-}
